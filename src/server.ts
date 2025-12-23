1e6b1079e73aca141caaca84d2c5e93bc265abd6
vbui <vbui@axon.com>
2025-12-23 16:05:44 +0700

    setup

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,24 @@
 
+import * as dotenv from 'dotenv';
+
+const result = dotenv.config();
+
+if (result.error) {
+    console.warn("No .env file found or error in loading .env file");
+    process.exit(1);
+}
+
+console.log(process.env.PORT);
+
 import * as express from 'express';
 import { root } from './route/root';
+import { isInteger } from './utils';
 
 const app = express();
 
 function setupExpress(){
+
+   
     
     app.route("/").get(root);
 
@@ -12,8 +26,25 @@ function setupExpress(){
 }
 
 function startServer(){
-    app.listen(9000,()=>{
-        console.log("Server is running on http://localhost:9000");
+
+    let port:number;
+
+    const portEnv = process.env.PORT;
+    const  portArg = process.argv[2]
+    
+    if (isInteger(portEnv)){
+        port = parseInt(portEnv)
+    }
+
+    if (!port && isInteger(portArg)){
+        port = parseInt(portArg);
+    }
+
+    if (!port){
+        port = 9000;
+    }
+    app.listen(port,()=>{
+        console.log(`Server is running on http://localhost:${port}`);
     })
 }
 
