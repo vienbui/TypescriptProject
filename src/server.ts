c298952a9009258f9cc10956a2d38af75fd2560a
vbui <vbui@axon.com>
2025-12-26 15:14:11 +0700

    create Course endpoint

--- src/server.ts
+++ src/server.ts
@@ -21,6 +21,7 @@ import { globalErrorHandler } from './middlewares/default-error-handler';
 import {findLessonsForCourse} from './route/find-lesson-for-course';
 import {getAllCoursesWithLessons} from './route/get-all-courses-with-lessons'
 import { updateCourse } from './route/update-course';
+import { createCourse } from './route/create-course';
 
 // const cors = require ("cors")
 
@@ -33,13 +34,20 @@ function setupExpress(){
     app.use(bodyParser.json());
 
     app.route("/").get(root);
+
     app.route("/api/courses").get(getAllCourses);
+
     app.route("/api/courses-include-lessons").get(getAllCoursesWithLessons);
+
     app.route("/api/courses/url/:courseUrl").get(findCourseByUrl);
+
     app.route("/api/courses/id/:courseId").get(findCourseById);
+
     app.route("/api/courses/id/:courseId/lessons").get(findLessonsForCourse);
 
     app.route("/api/courses/id/:courseId").patch(updateCourse);
+
+    app.route("/api/courses").post(createCourse);
 
     app.use(globalErrorHandler);
 
