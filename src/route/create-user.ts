f78ade311328ae59f08e0b1c5ce08d534db2b303
vbui <vbui@axon.com>
2025-12-27 17:23:09 +0700

    create  user endpoint

--- src/route/create-user.ts
+++ src/route/create-user.ts
@@ -2,8 +2,6 @@ import {NextFunction, Request, Response} from 'express';
 import { logger } from '../logger';
 import { AppDataSource } from '../database/data-source';
 import { User } from '../entitites/user';
-import { error } from 'winston';
-import { randomBytes } from 'crypto';
 import { calculatePasswordHash } from '../utils';
 
 const crypto = require("crypto");
@@ -11,18 +9,24 @@ const crypto = require("crypto");
 export async function createUser(request: Request, response: Response, next: NextFunction) {
 
 try {
-    const { email, pictureUrl, password, plainTextPassword, isAdmin } = request.body;
+    logger.debug("RAW request.body =", request.body);
 
-    logger.debug("create User() called");
+    const { email, pictureUrl, password,  isAdmin } = request.body;
 
-    
+    logger.debug("create User() called");       
 
     if (!email) {
-        throw "Could not extract the email from the request"
+        return response.status(400).json({
+        status: "fail",
+        message: "Could not extract the email from the request"
+    })
     }
        
     if (!password) {
-        throw "Could not extract the password from the request"
+        return response.status(400).json({
+        status: "fail",
+        message: "password is required"
+    })
     }
 
     const repository = await AppDataSource.getRepository(User)
@@ -31,15 +35,18 @@ try {
                                 .where("email = :email", {email})
                                 .getOne();
     if (user){
-        const message = ` User with email ${email} already exists`;
-        logger.error(message);
-        response.status(500).json({message});
-        return;
+        return response.status(409).json({
+        status:"fail",
+        message: ` User with email ${email} already exists`,
+        })
     }
 
     const passwordSalt = crypto.randomBytes(64).toString('hex')
 
     const passwordHash = await calculatePasswordHash(password, passwordSalt)
+
+    logger.debug("Request body", request.body);
+
 
     const newUser = repository.create({
         email, 
@@ -61,6 +68,6 @@ try {
 
 }
 catch (error) {
-    logger.error("Error in createCourse", error);
+    logger.error("Error in create User", error);
     return next(error);
 }}
